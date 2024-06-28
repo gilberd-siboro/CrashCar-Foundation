685bc987df439888b1518f169f3dee623c5ad551
Gilberd Siboro <gsiboro11016@gmail.com>
2024-06-28 16:39:50 +0700

    Initial Commit

---
+++ CrashCar-Foundation/GameOverScene.swift
@@ -0,0 +1,46 @@
+//
+//  GameOverScene.swift
+//  CrashCar-Foundation
+//
+//  Created by Foundation-010 on 28/06/24.
+//
+
+import Foundation
+import SpriteKit
+import UIKit
+
+
+class GameOverScene: SKScene {
+    var gameOverLabel: SKLabelNode?
+    var restartLabel: SKLabelNode?
+    
+    
+    override func didMove(to view: SKView) {
+        restartLabel = self.childNode(withName: "//restartLabel") as? SKLabelNode
+        gameOverLabel = SKLabelNode(text: "GAME OVER")
+        gameOverLabel?.fontSize = 50
+        gameOverLabel?.position = .zero
+        
+        addChild(gameOverLabel!)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // check which node it touched
+        
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        
+        if self.atPoint(location) == restartLabel{
+            restartGame()
+        }
+    }
+    
+    func restartGame(){
+        if let scene = SKScene(fileNamed: "GameScene"){
+            scene.scaleMode = .aspectFill
+            
+            let transition = SKTransition.doorway(withDuration: 1)
+            view?.presentScene(scene, transition: transition)
+        }
+    }
+}
